d591e1755c26826ab323472e15185676d45174c1
Tran Son (チャン・ソン) <t.son@verve-inc.jp>
2017-10-13 13:29:11 +0900

    [Command]: Send price tracking message via Slack webhook.

--- app/Console/Commands/MailTicker.php
+++ app/Console/Commands/MailTicker.php
@@ -5,7 +5,6 @@ namespace App\Console\Commands;
 use App\Models\Ticker;
 use App\Models\User;
 use Illuminate\Console\Command;
-use Illuminate\Support\Facades\Mail;
 
 class MailTicker extends Command
 {
@@ -41,35 +40,51 @@ class MailTicker extends Command
     public function handle()
     {
         foreach (Ticker::all() as $ticker) {
-	    $data = json_decode(file_get_contents('https://api.kraken.com/0/public/Ticker?pair=' . $ticker->pair), true);
+            $data = json_decode(file_get_contents('https://api.kraken.com/0/public/Ticker?pair=' . $ticker->pair), true);
             $flatten = function ($value) {
                 return is_array($value) ? implode(Ticker::DELIMITER, $value) : $value;
             };
             $result = array_map($flatten, reset($data['result']));
-            $save = function ($ticker, $result) {
-                $ticker->ask = $result['a'];
-                $ticker->bid = $result['b'];
-                $ticker->closed = $result['c'];
-                $ticker->volume = $result['v'];
-                $ticker->price = $result['p'];
-                $ticker->trades = $result['t'];
-                $ticker->low = $result['l'];
-                $ticker->high = $result['h'];
-                $ticker->opening = $result['o'];
-                $ticker->save();
-            };
             if ($ticker->opening !== '') {
                 $closed = $ticker->compare($result, 'closed');
                 $threshold = $ticker->threshold;
                 if (abs($closed) * 100 >= $threshold) { // Percentage
-                    foreach (User::all() as $user) {
-                        Mail::to($user->email)->send(new \App\Mail\Ticker($ticker, $result));
-                    }
-                    $save($ticker, $result);
+                    $url = env('SLACK_WEBHOOK_URL', '');
+                    $text = 'Kraken: ' . $ticker->pair .
+                        '. Sentiment: ' . number_format($ticker->compare($result, 'closed') * 100, 2) .
+                        '%. New closed: ' . number_format(floatval($result['c']), 2) . '.';
+                    $message = json_encode(array(
+                        'text' => $text
+                    ));
+                    $curl = curl_init($url);
+                    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $message);
+                    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                        'Content-Type: application/json',
+                        'Content-Length: ' . strlen($message)
+                    ));
+                    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+                    curl_exec($curl);
+                    curl_close($curl);
+                    $this->save($ticker, $result);
                 }
             } else {
-                $save($ticker, $result);
+                $this->save($ticker, $result);
             }
         }
+    }
+
+    private function save($ticker, $result)
+    {
+        $ticker->ask = $result['a'];
+        $ticker->bid = $result['b'];
+        $ticker->closed = $result['c'];
+        $ticker->volume = $result['v'];
+        $ticker->price = $result['p'];
+        $ticker->trades = $result['t'];
+        $ticker->low = $result['l'];
+        $ticker->high = $result['h'];
+        $ticker->opening = $result['o'];
+        $ticker->save();
     }
 }
